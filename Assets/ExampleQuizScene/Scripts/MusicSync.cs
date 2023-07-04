9272b11779b91478b4a328adb6a5c861fae241b4
Fax <9169275+daalta@users.noreply.github.com>
2023-07-04 23:27:54 +0200

    Sets up package

---
+++ Assets/ExampleQuizScene/Scripts/MusicSync.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Utility
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class MusicSync : UdonSharpBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+
+        private void OnEnable()
+        {
+            //Debug.Log("Super VR Ball - The server time is " + Networking.GetServerTimeInSeconds());
+            var newTime = ((float) Networking.GetServerTimeInSeconds()) % audioSource.clip.length;
+            audioSource.time = newTime + (newTime < 0 ? audioSource.clip.length : 0);
+            audioSource.Play();
+            
+            //Debug.Log("Super VR Ball - Synced music playback to " + audioSource.time + ". Intended: " +newTime);
+        }
+    }
+}
